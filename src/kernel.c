9d5d75244a7882f9b986960451e5a7d7df07a782
Minh Phan <minh40490@gmail.com>
2023-09-03 11:31:36 +0700

    update: color for group name

--- src/kernel.c
+++ src/kernel.c
@@ -4,7 +4,7 @@
 
 void printName() {
 	// Background color
-	drawRectARGB32(0, 0, 1024, 768, 0x00FF0000, 1);
+	drawRectARGB32(0, 0, 1024, 768, 0x00B8C3E3, 1);
 
 	//Upper case test
 	drawLetter('A', 40, 40, 0x00634B95);
@@ -97,64 +97,64 @@ void printName() {
 	// drawLetter('@', 320, 360, 0x00634B95);
 
 	// //Group name
-	// drawLetter('P', 20, 20, 0x00634B95);
-	// drawLetter('h', 60, 20, 0x00634B95);
-	// drawLetter('a', 100, 20, 0x00634B95);
-	// drawLetter('n', 140, 20, 0x00634B95);
-	// drawLetter('N', 200, 20, 0x00634B95);
-	// drawLetter('h', 240, 20, 0x00634B95);
-	// drawLetter('a', 280, 20, 0x00634B95);
-	// drawLetter('t', 320, 20, 0x00634B95);
-	// drawLetter('M', 380, 20, 0x00634B95);
-	// drawLetter('i', 420, 20, 0x00634B95);
-	// drawLetter('n', 460, 20, 0x00634B95);
-	// drawLetter('h', 500, 20, 0x00634B95);
+	// drawLetter('P', 20, 20, 0x009400D3);
+	// drawLetter('h', 60, 20, 0x004B0082);
+	// drawLetter('a', 100, 20, 0x000000FF);
+	// drawLetter('n', 140, 20, 0x0000FF00);
+	// drawLetter('N', 200, 20, 0x00FFFF00);
+	// drawLetter('h', 240, 20, 0x00FF7F00);
+	// drawLetter('a', 280, 20, 0x00FF0000);
+	// drawLetter('t', 320, 20, 0x009400D3);
+	// drawLetter('M', 380, 20, 0x004B0082);
+	// drawLetter('i', 420, 20, 0x000000FF);
+	// drawLetter('n', 460, 20, 0x0000FF00);
+	// drawLetter('h', 500, 20, 0x00FFFF00);
 
-	// drawLetter('T', 20, 80, 0x00634B95);
-	// drawLetter('r', 60, 80, 0x00634B95);
-	// drawLetter('a', 100, 80, 0x00634B95);
-	// drawLetter('n', 140, 80, 0x00634B95);
-	// drawLetter('M', 200, 80, 0x00634B95);
-	// drawLetter('a', 240, 80, 0x00634B95);
-	// drawLetter('i', 280, 80, 0x00634B95);
-	// drawLetter('N', 360, 80, 0x00634B95);
-	// drawLetter('h', 400, 80, 0x00634B95);
-	// drawLetter('u', 440, 80, 0x00634B95);
-	// drawLetter('n', 480, 80, 0x00634B95);
-	// drawLetter('g', 520, 80, 0x00634B95);
+	// drawLetter('T', 20, 80, 0x00FF7F00);
+	// drawLetter('r', 60, 80, 0x00FF0000);
+	// drawLetter('a', 100, 80, 0x009400D3);
+	// drawLetter('n', 140, 80, 0x004B0082);
+	// drawLetter('M', 200, 80, 0x000000FF);
+	// drawLetter('a', 240, 80, 0x0000FF00);
+	// drawLetter('i', 280, 80, 0x00FFFF00);
+	// drawLetter('N', 360, 80, 0x00FF7F00);
+	// drawLetter('h', 400, 80, 0x00FF0000);
+	// drawLetter('u', 440, 80, 0x009400D3);
+	// drawLetter('n', 480, 80, 0x004B0082);
+	// drawLetter('g', 520, 80, 0x000000FF);
 
-	// drawLetter('N', 20, 140, 0x00634B95);
-	// drawLetter('g', 60, 140, 0x00634B95);
-	// drawLetter('u', 100, 140, 0x00634B95);
-	// drawLetter('y', 140, 140, 0x00634B95);
-	// drawLetter('e', 180, 140, 0x00634B95);
-	// drawLetter('n', 220, 140, 0x00634B95);
-	// drawLetter('N', 280, 140, 0x00634B95);
-	// drawLetter('g', 320, 140, 0x00634B95);
-	// drawLetter('o', 360, 140, 0x00634B95);
-	// drawLetter('c', 400, 140, 0x00634B95);
-	// drawLetter('M', 460, 140, 0x00634B95);
-	// drawLetter('i', 500, 140, 0x00634B95);
-	// drawLetter('n', 540, 140, 0x00634B95);
-	// drawLetter('h', 580, 140, 0x00634B95);
+	// drawLetter('N', 20, 140, 0x0000FF00);
+	// drawLetter('g', 60, 140, 0x00FFFF00);
+	// drawLetter('u', 100, 140, 0x00FF7F00);
+	// drawLetter('y', 140, 140, 0x00FF0000);
+	// drawLetter('e', 180, 140, 0x009400D3);
+	// drawLetter('n', 220, 140, 0x004B0082);
+	// drawLetter('N', 280, 140, 0x000000FF);
+	// drawLetter('g', 320, 140, 0x0000FF00);
+	// drawLetter('o', 360, 140, 0x00FFFF00);
+	// drawLetter('c', 400, 140, 0x00FF7F00);
+	// drawLetter('M', 460, 140, 0x00FF0000);
+	// drawLetter('i', 500, 140, 0x009400D3);
+	// drawLetter('n', 540, 140, 0x004B0082);
+	// drawLetter('h', 580, 140, 0x000000FF);
 
-	// drawLetter('T', 20, 200, 0x00634B95);
-	// drawLetter('r', 60, 200, 0x00634B95);
-	// drawLetter('a', 100, 200, 0x00634B95);
-	// drawLetter('n', 140, 200, 0x00634B95);
-	// drawLetter('N', 200, 200, 0x00634B95);
-	// drawLetter('g', 240, 200, 0x00634B95);
-	// drawLetter('u', 280, 200, 0x00634B95);
-	// drawLetter('y', 320, 200, 0x00634B95);
-	// drawLetter('e', 360, 200, 0x00634B95);
-	// drawLetter('n', 400, 200, 0x00634B95);
-	// drawLetter('H', 460, 200, 0x00634B95);
-	// drawLetter('a', 500, 200, 0x00634B95);
-	// drawLetter('K', 560, 200, 0x00634B95);
-	// drawLetter('h', 600, 200, 0x00634B95);
-	// drawLetter('a', 640, 200, 0x00634B95);
-	// drawLetter('n', 680, 200, 0x00634B95);
-	// drawLetter('h', 720, 200, 0x00634B95);
+	// drawLetter('T', 20, 200, 0x0000FF00);
+	// drawLetter('r', 60, 200, 0x00FFFF00);
+	// drawLetter('a', 100, 200, 0x00FF7F00);
+	// drawLetter('n', 140, 200, 0x00FF0000);
+	// drawLetter('N', 200, 200, 0x009400D3);
+	// drawLetter('g', 240, 200, 0x004B0082);
+	// drawLetter('u', 280, 200, 0x000000FF);
+	// drawLetter('y', 320, 200, 0x0000FF00);
+	// drawLetter('e', 360, 200, 0x00FFFF00);
+	// drawLetter('n', 400, 200, 0x00FF7F00);
+	// drawLetter('H', 460, 200, 0x00FF0000);
+	// drawLetter('a', 500, 200, 0x009400D3);
+	// drawLetter('K', 560, 200, 0x004B0082);
+	// drawLetter('h', 600, 200, 0x000000FF);
+	// drawLetter('a', 640, 200, 0x0000FF00);
+	// drawLetter('n', 680, 200, 0x00FFFF00);
+	// drawLetter('h', 720, 200, 0x00FF7F00);
 }
 
 void main()
