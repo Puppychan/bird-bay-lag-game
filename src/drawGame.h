bfdd4562bbd6212a97ee6fad62bdd7eff3faa261
Nhung <nhungmaitran1412@gmail.com>
2023-09-26 15:46:27 +0700

    Increase movement

--- src/drawGame.h
+++ src/drawGame.h
@@ -6,7 +6,7 @@
 #define DEFAULT_BACKGROUND 0
 #define DEFAULT_BIRD 0
 
-#define FRAME_MOVING_GAME 1000/8
+#define FRAME_MOVING_GAME 1000/8.5
 // Constants for the game mechanics
 // #define pipes_size 10
 
